8cbbf935866fb3e5d86ec6e12e750ec0ca4aa9ea
Monet <codem0n3t@gmail.com>
2024-12-01 03:01:02 -0500

    added: validation.js for modal

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,17 +1,44 @@
-const checkInputValidity = (formElement, inputElement) => {
+const showInputError = (formElement, inputElement, errorMsg) => {
+  const errorMsgID = inputElement.id + "-error";
+  const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = errorMsg;
+};
+
+const checkInputValidity = (formElement, inputElement, config) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      config
+    );
+  } else {
+    hideInputError(formElement, inputElement, config);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement, config) => {
+  if (hasInvalidInput(inputList)) {
+    disableButton(buttonElement, config);
+  } else {
+    enableButton(buttonElement, config);
   }
 };
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
-  const buttonElement = formElement.querySelectorAll(".modal__submit-btn");
+  const buttonElement = formElement.querySelector(".modal__submit-btn");
 
   // toggleButtonState(inputList, buttonElement);
 
   inputList.forEach((inputElement) => {
-    inputElement.addEventListeners("input", function () {
+    inputElement.addEventListener("input", function () {
       checkInputValidity(formElement, inputElement);
       // toggleButtonState(inputList, buttonElement);
     });
