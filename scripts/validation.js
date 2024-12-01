06952f43a42e79c00bc73cdc28d7df21e165920f
Monet <codem0n3t@gmail.com>
2024-12-01 01:30:30 -0500

    added: validation.js for modal

---
+++ scripts/validation.js
@@ -0,0 +1,28 @@
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
+  const buttonElement = formElement.querySelectorAll(".modal__submit-btn");
+
+  // toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListeners("input", function () {
+      checkInputValidity(formElement, inputElement);
+      // toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
